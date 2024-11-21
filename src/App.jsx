7a9871885ed0fdbd5dc16ee0ca493cc6ef8d523b
SamadhanTikone <99667605+SamadhanTikone@users.noreply.github.com>
2024-11-21 19:26:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
 
 import './App.css'
+import About from "./Blogs/About";
+import { createRoot } from 'react-dom/client'
+import NewBlog from "./Blogs/NewBlog";
+import Layout from "./Blogs/Layout";
+import { createBrowserRouter , RouterProvider } from "react-router-dom";
+
+import './index.css'
+import Home from './Blogs/Home.jsx';
+import BlogDetails from './Blogs/BlogDetails.jsx';
+import Error from './Blogs/Error.jsx';
+
+
+
 
 function App() {
 
+
+  const router = createBrowserRouter([
+  { path : "/",
+   element : <Layout/>,
+   errorElement: <Error/>,
+   children : [
+    {
+      path: "",
+      element: <Home/>
+
+    },
+     {
+     path : "/about",
+     element : <About/>,
+     },
+     {
+       path: "/newBlog",
+       element: <NewBlog/>, 
+     },
+     {
+       path : "/blogs/:id",
+       element: <BlogDetails/>,
+     }
+   ]
+ }
+ ])
+
+
+  
   return (
     <>   
-    
+       <RouterProvider router={router}/>
     </>
   )
 }
